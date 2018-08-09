2cb918b8d15b11e317c1dbb8ac997462660baf62
yuebinyun <yuebinyun@gmail.com>
2018-08-09 23:53:19 +0800

    init glib demo

--- glib-demo/main.c
+++ glib-demo/main.c
@@ -2,10 +2,20 @@
 #include <glib.h>
 
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-result"
 int main() {
     GList *list = NULL;
-    list = g_list_append(list, "Hello, World!\n");
-    printf("The first item is %s", (char *) g_list_first(list)->data);
+
+    list = g_list_append(list, "Hello, World!");
+    printf("The first item is %s\n", (char *) g_list_first(list)->data);
+    printf("The length of list is %d\n", g_list_length(list));
+
+    g_list_append(list, "Good Morning!");
+    printf("The length of list is %d\n", g_list_length(list));
+    printf("The last item is %s\n", (char *) g_list_last(list)->data);
+
     g_list_free(list);
     return 0;
 }
+#pragma clang diagnostic pop
